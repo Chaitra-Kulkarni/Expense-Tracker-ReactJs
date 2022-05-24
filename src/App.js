d40bc745aac44c70a4426048d07b09f39883f143
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-05-24 08:28:35 +0530

    filtered Data

--- src/App.js
+++ src/App.js
@@ -7,31 +7,43 @@ const DUMMY_EXPENSES = [
     id: 1,
     title: 'New TV',
     amount: 100,
-    date: new Date(2022, 7, 14),
+    date: new Date(2022, 7, 14)
   },
   { 
     id: 2, 
     title: 'Trash Bags', 
     amount: 1, 
-    date: new Date(2022, 2, 12) 
+    date: new Date(2021, 2, 12) 
   },
   {
     id: 3,
     title: 'Car Insurance',
     amount: 294,
-    date: new Date(2022, 2, 28),
+    date: new Date(2021, 2, 28)
   },
   {
     id: 4,
     title: 'New Desk (Wooden)',
     amount: 45,
-    date: new Date(2022, 5, 12),
+    date: new Date(2022, 5, 12)
   },
   {
     id: 5,
     title: 'Washing Machine',
     amount: 145,
-    date: new Date(2022, 5, 12),
+    date: new Date(2020, 3, 19)
+  },
+  {
+    id: 6,
+    title: 'Health Insurance',
+    amount: 245,
+    date: new Date(2019, 12, 29)
+  },
+  {
+    id: 7,
+    title: 'New Fridge',
+    amount: 55,
+    date: new Date(2019, 11, 30)
   }
 ]
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,18 +6,33 @@ import './Expenses.css';
 
 function Expenses(props) {
 
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2022');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
 
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-        {props.items.map(item => <ExpenseItem title={item.title} amount={item.amount} date={item.date} />)}
+        {filteredExpenses.map(item => <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />)}
+
+        {/* {props.items.map((expense) => (
+          expense.date.getFullYear() == filteredYear) ?
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          ></ExpenseItem> : null
+        )} */}
+
       </Card>
     </div>
   );
